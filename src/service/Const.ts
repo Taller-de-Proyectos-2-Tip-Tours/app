0f9db2aa75f6968e0e132706e500ef721a67932c
Gerónimo Illescas <gillescas@fi.uba.ar>
2023-09-24 19:52:31 -0300

    Use backend endpoint
    Signed-off-by: Gerónimo Illescas <gillescas@fi.uba.ar>

--- src/service/Const.ts
+++ src/service/Const.ts
@@ -1,6 +1,6 @@
 
 
-export const API_URL = 'http://192.168.1.57:6000';
+export const API_URL = 'https://tdp2-backend.onrender.com';
 
 export const API_URL_TOURS = `${API_URL}/tours`;
 export const API_URL_CITIES = `${API_URL}/cities`;
