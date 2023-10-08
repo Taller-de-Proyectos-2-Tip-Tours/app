e2451279e5ca74f31dee0c3bf4b61e2e117cd474
Geronimo Illescas <gillescas@fi.uba.ar>
2023-10-08 19:06:59 -0300

    Add empty space to reserves list

--- src/components/screens/ReservesScreen.tsx
+++ src/components/screens/ReservesScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import { getReserves } from "../../useCases/getReservesUseCase";
 import { ReserveList } from "../ReserveList";
 import Spinner from "react-native-loading-spinner-overlay";
@@ -10,38 +10,43 @@ export default function ReservesScreen({ route }) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  useFocusEffect(React.useCallback(() => {
-    setLoading(true);
+  useFocusEffect(
+    React.useCallback(() => {
+      setLoading(true);
 
-    getReserves()
-      .then((data) => {
-        setData(data);
-        console.log(data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.log(err);
-        setError("Hubo un error cargando los datos :(");
-        setLoading(false);
-      });
-  }, []));
+      getReserves()
+        .then((data) => {
+          setData(data);
+          console.log(data);
+          setLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          setError("Hubo un error cargando los datos :(");
+          setLoading(false);
+        });
+    }, [])
+  );
 
   return (
     <View style={styles.container}>
       {loading ? (
         <Spinner
           visible={loading}
-          textContent={'Cargando...'}
+          textContent={"Cargando..."}
           textStyle={{ color: "white" }}
-          />
+        />
       ) : error ? (
-        <View style={styles.textContainer}> 
+        <View style={styles.textContainer}>
           <Text>Error: {error}</Text>
         </View>
-        
       ) : data.length == 0 ? (
-        <View style={styles.textContainer}> 
-        <Text  style={styles.textContainer}>No hay reservas hechas</Text>
+        <View style={styles.textContainer}>
+          <Image
+            style={styles.thumbail}
+            source={require("../../../assets/leaf.png")}
+          />
+          <Text>No hay reservas hechas</Text>
         </View>
       ) : (
         <ReserveList style={{ flex: 3 }} tours={data} />
@@ -53,10 +58,15 @@ export default function ReservesScreen({ route }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "white"
+    backgroundColor: "white",
   },
   textContainer: {
+    flex: 1,
     alignItems: "center",
     justifyContent: "center",
-  }
+  },
+  thumbail: {
+    width: 150,
+    height: 150,
+  },
 });
